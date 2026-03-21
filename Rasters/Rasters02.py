9e06144a3920e54f4132cc3191851c7c8b1ee2ce
Carter Moore <cartermoore@utexas.edu>
2026-03-21 12:35:52 -0500

    Made file handling a bit easier to use

--- Rasters/Rasters02.py
+++ Rasters/Rasters02.py
@@ -1,33 +1,26 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import os
+from pathlib import Path
 import rasterFunctions as rf
 from joblib import Parallel, delayed
 import time
 
 ## To do: count total spikes for each neuron first, that way we can allocate smaller arrays and
 #           in the case of granule cells we can avoid loading cells that don't fire..
-rasterFilename = 'MFtest08b.mfr'      # Name of the raster file from the simulation, If it's the first load for this file then it'll load
-                                # If you've already loaded this file before then it'll read the loaded data from file.
-saveFilename = "MFTest08bRasters"   # Name of file for strored rasters, 
+rasterFilepath = Path(input("Enter absolute filepath: "))
+saveFilepath = rasterFilepath.with_suffix('.npy')   # Name of file for strored rasters, 
 showNeuron = [0,1,2]                  # Use this if you want to look at rasters for one particular neurons. = 0 defaults to view in sequence
-                                # Can also be a list of neurons
-maxSpikesPerTrial = 1000        # This has to be larger than the most spikes any of the neurons would have in one trial
-cellType, numNeurons = rf.determineCellType(rasterFilename)
+maxSpikesPerTrial = 5000        # This has to be larger than the most spikes any of the neurons would have in one trial
+cellType, numNeurons = rf.determineCellType(str(rasterFilepath))
 
-currentPath = os.getcwd()
-Filename = currentPath + "\\" + saveFilename
-savedFilename = Filename + ".npy"
 
 total_start = time.time()
-if os.path.exists(savedFilename): # no need to read in data again, upload and plot
-    result_rasters = np.load(savedFilename)
-else:  # Read in data first and save to file in format that's faster to read next time
+if rasterFilepath.is_file():
     print("Reading data...")
-    if ((rasterFilename[-3:])!="grr"):
-        data = np.fromfile(rasterFilename,dtype=np.int16)
+    if ((rasterFilepath.suffix)!="grr"):
+        data = np.fromfile(str(rasterFilepath),dtype=np.int16)
     else:
-        data = np.fromfile(rasterFilename,dtype=np.int32)
+        data = np.fromfile(str(rasterFilepath),dtype=np.int32)
     # split into by trial 1D arrays
     print("Splitting Raster to Trials...")
     split_start = time.time()
@@ -39,10 +32,14 @@ else:  # Read in data first and save to file in format that's faster to read nex
     main_start = time.time()
     result_rasters = Parallel(n_jobs=-1)(delayed(rf.process_trial)(vec, numNeurons, maxSpikesPerTrial) for vec in data_byTrial)
     result_rasters = np.array(result_rasters)
+    print(result_rasters.shape)
     main_end = time.time()
     print(f"Time: {main_end - main_start:.2f}s")
-    np.save(Filename, result_rasters)
+    print(f"Saving file to: {str(saveFilepath)}")
+    np.save(str(saveFilepath), result_rasters)
     total_end = time.time()
     print(f"Total Time: {total_end-total_start:.2f}s")
+else: 
+    print("File not found")
 # plot the rasters for each neuron, or selected neurons
-rf.plot_rasters(result_rasters, numNeurons, showNeuron)
+#rf.plot_rasters(result_rasters, numNeurons, showNeuron)
